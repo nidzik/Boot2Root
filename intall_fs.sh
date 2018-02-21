fb87b9fdf0e11c03ce5e432e60e2ee7c789bd5c9
root <root@localhost.localdomain>
2018-02-21 19:44:27 +0000

    add run_zaz.sh

--- intall_fs.sh
+++ intall_fs.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 mkdir /mnt/iso
 mkdir /mnt/fs
-wget http://dl.42.fr/BornToSecHackMe-v1.1.iso /tmp
+cd /tmp
+wget http://dl.42.fr/BornToSecHackMe-v1.1.iso
 mount /tmp/BornToSecHackMe-v1.1.iso /mnt/iso
-mount /mnt/iso/casper/filesystem.squashfs /tmp/fs/ -t squash -o loop
+mount /mnt/iso/casper/filesystem.squashfs /mnt/fs/ -t squashfs -o loop
 wget -q -O - https://archive.kali.org/archive-key.asc  | apt-key add #get valid key 
-sudo apt-get install libc6-i386
-sudo apt-get install gcc-7-multilib
-cat /tmp/root-me/utils/install_peda.sh
-/tmp/root-me/utils/install_peda.sh
+sudo apt-get update
+sudo apt-get install libc6-i386 -y
+sudo apt-get install gcc-7-multilib -y
+cat /tmp/root-me/utils/peda/install_peda.sh
+/tmp/root-me/utils/peda/install_peda.sh
 
