18349df7224431bc998cdea752b1123d4a8ce985
Nicolas IDZIK <nidzik@e2r5p15.42.fr>
2018-02-21 19:38:28 +0100

    add valid key apt

--- intall_fs.sh
+++ intall_fs.sh
@@ -4,6 +4,7 @@ mkdir /mnt/fs
 wget http://dl.42.fr/BornToSecHackMe-v1.1.iso /tmp
 mount /tmp/BornToSecHackMe-v1.1.iso /mnt/iso
 mount /mnt/iso/casper/filesystem.squashfs /tmp/fs/ -t squash -o loop
+wget -q -O - https://archive.kali.org/archive-key.asc  | apt-key add #get valid key 
 sudo apt-get install libc6-i386
 sudo apt-get install gcc-7-multilib
 cat /tmp/root-me/utils/install_peda.sh
