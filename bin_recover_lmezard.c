7c203588b8c8396439e7d9196984b1ccf165430b
root <root@localhost.localdomain>
2018-02-21 21:00:04 +0000

    lmezar done.

---
+++ bin_recover_lmezard.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+char getme1() {
+	return 'I';
+}
+
+char getme2() {
+	return 'h';
+}
+
+char getme3() {
+	return 'e';
+}
+
+char getme4() {
+	return 'a';
+}
+
+char getme5() {
+	return 'r';
+}
+
+char getme6() {
+	return 't';
+}
+
+char getme7() {
+	return 'p';
+}
+
+char getme8() {
+	return 'w';
+}
+
+char getme9() {
+	return 'n';
+}
+
+char getme10() {
+	return 'a';
+}
+
+char getme11() {
+	return 'g';
+}
+
+char getme12()
+{
+	return 'e';
+}
+/*1-e 	2-
+7DT5Q.pcap:	return 'a';
+APM1E.pcap:	return 'I';
+ECOW1.pcap:	return 'e';
+J5LKW.pcap:	return 't';
+T44J5.pcap:	return 'p';
+T7VV0.pcap:	return 'r';
+ZPY1Q.pcap:	return 'h';
+
+0T16C.pcap:	char getme4() {
+32O0M.pcap:	char getme7() {
+331ZU.pcap:	char getme1() {
+4KAOH.pcap:	char getme5() {
+91CD0.pcap:	char getme6() {
+B62N4.pcap:	char getme3() {
+*/
+int main() {
+        printf("M");
+        printf("Y");
+        printf(" ");
+        printf("P");
+        printf("A");
+        printf("S");
+        printf("S");
+        printf("W");
+        printf("O");
+        printf("R");
+        printf("D");
+        printf(" ");
+        printf("I");
+        printf("S");
+        printf(":");
+        printf(" ");
+        printf("%c",getme1());
+        printf("%c",getme2());
+        printf("%c",getme3());
+        printf("%c",getme4());
+        printf("%c",getme5());
+        printf("%c",getme6());
+        printf("%c",getme7());
+        printf("%c",getme8());
+        printf("%c",getme9());
+        printf("%c",getme10());
+        printf("%c",getme11());
+        printf("%c",getme12());
+        printf("\n");
+        printf("Now SHA-256 it and submit");
+
+        printf("\n");
+        printf("Now SHA-256 it and submit");
+}
